0fa37786b4e4df1a40006f7a0aa5cc52be5a92df
Daniel Papp <papp.daniel.gabor@gmail.com>
2023-01-13 18:47:33 +0100

    Add different error messages

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 const formValidator = (function() {
 
     const emailField = document.querySelector('#user-email');
+    const errorMsg = document.querySelector('.error-msg');
     const submitButton = document.querySelector('#submit-btn');
 
     const checkEmail = function() {
+
+        errorMsg.removeAttribute('class');
         
-        if (!emailField.checkValidity()) emailField.removeAttribute('placeholder');
+        if (!emailField.checkValidity() && emailField.value) {
+            
+            errorMsg.classList.add('error-format');
+
+        } else {
+
+            emailField.removeAttribute('placeholder');
+            errorMsg.classList.add('error-empty');
+
+        }
 
     }
 
     submitButton.addEventListener('click', checkEmail);
+    emailField.addEventListener('keydown', checkEmail);
 
 })();
